5e752a0a1dd9d615edd0352df4f36cf40bceb55d
dawnsqrl <1021938847dluim@gmail.com>
2025-05-04 01:43:13 -0400

    Update safety heatmap interpolation

--- js/component/visualization/safetyHeatmap.js
+++ js/component/visualization/safetyHeatmap.js
@@ -44,7 +44,7 @@ export function renderSafetyHeatmap(globalMap, stateName, county) {
 
       const scores = stateFiltered.map(getCrimeScore).filter((d) => d != null);
       const colorScale = d3
-        .scaleSequential()
+        .scaleSequentialSqrt()
         .domain([d3.min(scores), d3.max(scores)])
         .interpolator(d3.interpolateReds);
 
