9e0eb444f31b1aa1bf5a884724a3c3912b9dfeb0
dawnsqrl <1021938847dluim@gmail.com>
2025-05-04 00:01:37 -0400

    Update hotel dataset

--- data/hotel/split.py
+++ data/hotel/split.py
@@ -2,12 +2,11 @@ import re
 
 import pandas as pd
 
-# removed 'District of Columbia'
 states = [
     'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
-    'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho',
-    'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+    'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia',
+    'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+    'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
     'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada',
     'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina',
     'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
